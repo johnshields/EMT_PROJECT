3cc08a6b872e45917477cec1d0df19a10c6cd864
johnshields <G00348436@gmit.ie>
2021-01-05 02:17:12 +0000

    simple web service is set up
    web service is set up and connected to index.html. Dockerfile and requirements.txt have also been created

--- project_files/web-service/web-service.py
+++ project_files/web-service/web-service.py
@@ -1,10 +1,19 @@
 # Adapted from: https://flask.palletsprojects.com/en/1.1.x/quickstart/#a-minimal-application
+# https://web.microsoftstream.com/video/05d8b607-56c3-44a8-9539-c940257e43d8?referrer=https:%2F%2Flearnonline.gmit.ie%2F
+# https://web.microsoftstream.com/video/63704d51-e829-4da9-ab3d-ae73b71ee8f2
 
-from flask import Flask
+"""
+John Shields - G00348436
+"""
 
-app = Flask(__name__)
+# flask for web app
+import flask as fl
+
+# create a new web app
+app = fl.Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# add root route
+@app.route("/")
+def home():
+    return app.send_static_file('index.html')
